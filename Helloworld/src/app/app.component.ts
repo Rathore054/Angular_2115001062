3b7b0614752fd419ab26987900e21be24cea795e
Udit rathore <uditrathore054@gmail.com>
2025-03-29 19:51:29 +0530

    [Udit] Add : adding files to UC4

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -9,6 +9,28 @@ export class AppComponent {
   title = 'Helloworld';
   imgUrl="../assets/BL_logo_square_jpg.jpg";
   url="https://bridgelabz.com";
+  userName: string = "";
+
+  ngOnInit() : void {
+    this.title="Hello from BridgeLabz";
+  }
+
+  onClick($event){
+    console.log("Save button has been clicked!",$event);
+    window.open(this.url, "_blank");
+  }
+}import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Helloworld';
+  imgUrl="../assets/BL_logo_square_jpg.jpg";
+  url="https://bridgelabz.com";
+  userName: string = "";
 
   ngOnInit() : void {
     this.title="Hello from BridgeLabz";
